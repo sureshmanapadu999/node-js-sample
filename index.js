edce0f8cf493e741be36ae65ad221493d61c7987
sureshmanapadu999 <96465444+sureshmanapadu999@users.noreply.github.com>
2022-03-24 01:06:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('HELLO SURESH SOFTWARE TECH SOLUTIONS!')
+  response.send('HELLO SURESH SOFTWARE TECH SOLUTIONS AND HI U!')
 })
 
 app.listen(app.get('port'), function() {
